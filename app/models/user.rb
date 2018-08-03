1a85334122c899913321d9a5097e91145422d479
George Katsampu <g.katsiampas@skroutz.gr>
2018-08-04 00:14:18 +0300

    Add comments to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
 
 end
